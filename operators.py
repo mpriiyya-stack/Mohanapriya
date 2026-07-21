7043469040c7eb75c07c927153b031e9311bd857
mpriiyya-stack <mpriiyya@gmail.com>
2026-07-21 14:10:58 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,11 @@
+a = 10
+b = 5
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a == b:", a == b)
