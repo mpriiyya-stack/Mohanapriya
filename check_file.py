81464af6ebc07e2d8fa33bc8808b834da7054e73
Mohanapriya <mpriiyya@gmail.com>
2026-07-27 17:44:48 +0530

    fix-a change

---
+++ check_file.py
@@ -0,0 +1,9 @@
+import os
+if os.path.exists("input.txt"):
+    print("File found!")
+    with open("input.txt", "r") as f:
+        content = f.read()
+    print(content)
+else:
+    print("File does not exist.")
+    
